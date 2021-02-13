37e7f9bfae69fd75deeaf522c485d9ea318b5b11
1stminhcar <77107580+1stminhcar@users.noreply.github.com>
2021-02-13 16:13:29 +0700

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -20,7 +20,7 @@ module.exports = {
       return message.reply(i18n.__("volume.errorNotValid")).catch(console.error);
 
     queue.volume = args[0];
-    queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
+    queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 10000);
     return queue.textChannel.send(i18n.__mf("volume.result", { arg: args[0] })).catch(console.error);
   }
 };;
